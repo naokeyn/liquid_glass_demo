c7dbb915eac60c852b1a6f4096c39f3ace674c1e
naokeyn <89127745+naokeyn@users.noreply.github.com>
2025-11-02 23:06:15 +0900

    feat: implement ControlPanel for dynamic alpha adjustment

---
+++ lib/control_panel.dart
@@ -0,0 +1,43 @@
+import 'package:app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ControlPanel extends ConsumerStatefulWidget {
+  const ControlPanel({super.key, required this.scrollController});
+  final ScrollController scrollController;
+
+  @override
+  ConsumerState<ControlPanel> createState() => _ControlPanelState();
+}
+
+class _ControlPanelState extends ConsumerState<ControlPanel> {
+  int sliderValue = 0x33;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withAlpha(200),
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      child: ListView(
+        controller: widget.scrollController,
+        shrinkWrap: true,
+        children: [
+          Slider(
+            min: 0x00,
+            max: 0xFF,
+            value: sliderValue.toDouble(),
+            divisions: 0xFF,
+            label: 'Alpha: 0x${sliderValue.toRadixString(16).toUpperCase().padLeft(2, '0')}',
+            onChanged:
+                (value) => {
+                  sliderValue = value.toInt(),
+                  ref.read(alphaProvider.notifier).state = sliderValue,
+                },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:app/control_panel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:liquid_glass_renderer/liquid_glass_renderer.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
+
+final alphaProvider = StateProvider<int>((ref) => 0x33);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,20 +25,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final int minAlpha = 0x00;
-  final int maxAlpha = 0xFF;
-
-  int _currentSliderValue = 0x33;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int alpha = ref.watch(alphaProvider);
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
@@ -47,41 +45,29 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           // The LiquidGlassLayer manages glass rendering
           Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                LiquidGlassLayer(
-                  settings: LiquidGlassSettings(
-                    thickness: 20,
-                    blur: 10,
-                    glassColor: Color(_currentSliderValue << 24 | 0xFFFFFF),
-                  ),
-                  child: LiquidGlass(
-                    shape: LiquidRoundedSuperellipse(borderRadius: 50),
-                    child: const SizedBox(
-                      height: 200,
-                      width: 200,
-                      child: Center(child: FlutterLogo(size: 100)),
-                    ),
-                  ),
+            child: LiquidGlassLayer(
+              settings: LiquidGlassSettings(
+                thickness: 20,
+                blur: 10,
+                glassColor: Color(alpha << 24 | 0xFFFFFF),
+              ),
+              child: LiquidGlass(
+                shape: LiquidRoundedSuperellipse(borderRadius: 50),
+                child: const SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Center(child: FlutterLogo(size: 100)),
                 ),
-                const SizedBox(height: 40),
-                Slider(
-                  value: _currentSliderValue.toDouble(),
-                  onChanged: (value) {
-                    setState(() {
-                      _currentSliderValue = value.toInt();
-                    });
-                  },
-                  min: minAlpha.toDouble(),
-                  max: maxAlpha.toDouble(),
-                ),
-                Text(
-                  'Alpha: 0x${_currentSliderValue.toRadixString(16).padLeft(2, '0').toUpperCase()}',
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ],
+              ),
             ),
+          ),
+          DraggableScrollableSheet(
+            initialChildSize: 0.1,
+            minChildSize: 0.1,
+            maxChildSize: 0.5,
+            builder: (context, scrollController) {
+              return ControlPanel(scrollController: scrollController);
+            },
           ),
         ],
       ),
